69dc23822f6af2c21ced033773acbecc5c8faa20
Junha Kim <kimjh4930@gmail.com>
2021-02-10 07:21:12 +0000

    rock scissors paper

---
+++ rock-scissors-paper/RockScissorsPaper.vue
@@ -0,0 +1,47 @@
+<template>
+    <div>
+        <div id ="computer" :style="{ background: 'url(https://en.pimg.jp/023/182/267/1/23182267.jpg) ${imgCoord} 0'}"></div>
+        <div>
+            <button @click="onClickButton('바위')">바위</button>
+            <button @click="onClickButton('가위')">가위</button>
+            <button @click="onClickButton('보')">보</button>
+        </div>
+        <div>{{result}}</div>
+        <div>현재 {{score}}</div>
+        </div>
+</template>
+
+<script>
+const rspCoords = {
+        rock: '0',
+        scissors: '-142px',
+        paper : '-284px'
+    };
+
+export default {
+    
+    data() {
+        return {
+            imgCoord: rspCoords.rock,
+            result: '',
+            score: '',
+        }
+    },
+    computed: {
+        
+    },
+    methods: {
+        onClickButton (choice){
+            
+        },
+    },
+}
+</script>
+
+<style scoped>
+    #computer {
+        width: 142px;
+        height: 200px;
+        background-position: 0 0;
+    }
+</style>
---
+++ rock-scissors-paper/webpack.config.js
@@ -0,0 +1,48 @@
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    devtool: 'eval',
+    resolve: {
+        extensions: ['.js', '.vue']
+    },
+    entry: {
+        app: path.join(__dirname, 'main.js'),
+    },
+    module: {
+        rules: [{
+            test: /\.vue$/,
+            loader: 'vue-loader',
+        }, {
+            test: /\.css$/,
+            use: [
+                'vue-style-loader',
+                {
+                    loader: 'css-loader',
+                    options: {
+                        esModule: false,
+                    }
+                }
+
+            ]
+        }],
+    },
+
+    devServer: {
+        // historyApiFallback: true,
+        host: '192.168.0.21',
+        // publicPath: '/dist'
+        port: 8081
+    },
+
+    plugins: [
+        new VueLoaderPlugin(),
+    ],
+
+    output: {
+        filename: '[name].js',
+        path: path.join(__dirname, 'dist'),
+        publicPath: '/dist'
+    }
+}
